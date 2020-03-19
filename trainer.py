Fix head scaling

trainer.py
@@ -82,8 +82,9 @@ class Trainer:
                     self.model.emb.apply(self.model.clipper)
                 # Scale head so the output would always be a weighted average
                 with torch.no_grad():
-                    self.model.head.weight = torch.nn.Parameter(self.model.head.weight /
-                                                                torch.sum(self.model.head.weight), requires_grad=True)
+                    self.model.head.weight.div_(torch.sum(self.model.head.weight))
+                    # self.model.head.weight = torch.nn.Parameter(self.model.head.weight /
+                    #                                             torch.sum(self.model.head.weight), requires_grad=True)
 
                 # Print difference in correlation
                 correlation_after = self.get_prediction_correlation(tournament)